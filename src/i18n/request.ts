77c1aeff1999a16730c2473df73dd57946ed2a28
fulfill-tokuyama <t.tokuyama@fulfill-net.com>
2025-06-11 23:10:32 +0900

    変更

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  const locale = await requestLocale ?? 'ja';
+export default getRequestConfig(async () => {
+  const locale = 'en';
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: {}
   };
 }); 

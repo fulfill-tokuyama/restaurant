3f57c5ee522c1250b5c4cbecd3baa9903f013e2e
fulfill-tokuyama <t.tokuyama@fulfill-net.com>
2025-06-11 16:04:27 +0900

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,11 +1,9 @@
-import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-const locales = ['en', 'ja'] as const;
-
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as any)) notFound();
+export default getRequestConfig(async () => {
+  const locale = 'en';
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    locale,
+    messages: {}
   };
 }); 
